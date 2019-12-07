eb3804e459b538c6a009965f4a10ec8181273135
Tran Anh Thuan <33371625+anhthuan1999@users.noreply.github.com>
2019-12-08 03:27:12 +0700

    v

---
+++ script/scriptbestsales.js
@@ -0,0 +1,100 @@
+﻿function loading() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            HienThi(changetolistdata(this));
+        }
+    };
+    xhttp.open("GET", "../data/product.xml", true);
+    xhttp.send();
+    var lstproduct;
+    var xmlDoc;
+    var data = [];
+    function changetolistdata(xml) {
+
+        xmlDoc = xml.responseXML;
+
+        lstproduct = xmlDoc.getElementsByTagName('BS')[0].getElementsByTagName("product");
+        for (i = 0; i < lstproduct.length; i++) {
+            var product = lstproduct[i]
+            var detailproduct = []
+            detailproduct.push(product.getElementsByTagName("id")[0].childNodes[0].nodeValue,
+                product.getElementsByTagName("name")[0].childNodes[0].nodeValue,
+                product.getElementsByTagName("description")[0].childNodes[0].nodeValue,
+                product.getElementsByTagName("image")[0].childNodes[0].nodeValue,
+                product.getElementsByTagName("price")[0].childNodes[0].nodeValue
+            )
+            data.push(detailproduct)
+        }
+
+        return data;
+    }
+    function HienThi(lstrecord) {
+        tbody = document.getElementById("idmaintable")
+        numtdeachrow = 3;
+        widthimage = 300;
+        heightimage = 300;
+        for (i = 0; i < parseInt(lstrecord.length / numtdeachrow); i++) {
+                var tr = document.createElement("tr")
+                tbody.appendChild(tr)
+                var record1 = lstrecord[i * numtdeachrow]
+                var record2 = lstrecord[i * numtdeachrow + 1]
+                var record3 = lstrecord[i * numtdeachrow + 2]
+                var td1 = document.createElement("td")
+                var td2 = document.createElement("td")
+                var td3 = document.createElement("td")
+                tr.appendChild(td1)
+                tr.appendChild(td2)
+                tr.appendChild(td3)
+
+                var img1 = document.createElement("img")
+                img1.setAttribute("src", record1[3])
+                img1.setAttribute("width", widthimage)
+                img1.setAttribute("height", heightimage)
+                td1.appendChild(img1)
+
+
+                var name1 = document.createElement("p")
+                name1.innerText = "Tên: " + record1[1]
+                var des1 = document.createElement("p")
+                des1.innerText = "Mô tả: " + record1[2]
+                var price1 = document.createElement("p")
+                price1.innerText = "Giá: " + record1[4]
+                td1.appendChild(name1)
+                td1.appendChild(des1)
+                td1.appendChild(price1)
+
+                var img2 = document.createElement("img")
+                img2.setAttribute("src", record2[3])
+                img2.setAttribute("width", widthimage)
+                img2.setAttribute("height", heightimage)
+                td2.appendChild(img2)
+
+                var name2 = document.createElement("p")
+                name2.innerText = "Tên: " + record2[1]
+                var des2 = document.createElement("p")
+                des2.innerText = "Mô tả: " + record2[2]
+                var price2 = document.createElement("p")
+                price2.innerText = "Giá: " + record2[4]
+                td2.appendChild(name2)
+                td2.appendChild(des2)
+                td2.appendChild(price2)
+
+                var img3 = document.createElement("img")
+                img3.setAttribute("src", record3[3])
+                img3.setAttribute("width", widthimage)
+                img3.setAttribute("height", heightimage)
+                td3.appendChild(img3)
+
+                var name3 = document.createElement("p")
+                name3.innerText = "Tên: " + record3[1]
+                var des3 = document.createElement("p")
+                des3.innerText = "Mô tả: " + record3[2]
+                var price3 = document.createElement("p")
+                price3.innerText = "Giá: " + record3[4]
+                td3.appendChild(name3)
+                td3.appendChild(des3)
+                td3.appendChild(price3)
+        }
+    }
+}
---
+++ script/scriptstore.js
@@ -0,0 +1,81 @@
+﻿function defaultimg() {
+    p = document.getElementById("idlistdanhsach")
+    tmap = document.getElementById("idmap")
+    tmap.removeChild(tmap.firstChild);
+    var img = document.createElement('img')
+    tmap.appendChild(img)
+    img.setAttribute('height', '400')
+    img.setAttribute('width', '650')
+}
+function getValueSelect(v) {
+    p = document.getElementById("idlistdanhsach")
+    tmap = document.getElementById("idmap")
+    tmap.removeChild(tmap.firstChild);
+    var img = document.createElement('img')
+    tmap.appendChild(img)
+    
+    img.setAttribute('height', '400')
+    img.setAttribute('width', '650')
+    switch (v) {
+        case 'q1':
+            p.innerHTML = "Địa chỉ: Chung cư, 14 Tôn Thất Đạm, Phường Nguyễn Thái Bình, Quận 1, Quận 1 Hồ Chí Minh<br/>"+
+            "Giờ đóng cửa 22:00<br/>"+
+            "Điện thoại: 0786 575 464"
+            img.setAttribute('src','../images/mapq1.jpg')
+            break;
+        case 'q2':
+            p.innerHTML = "Địa chỉ: 10 Trần Ngọc Diện, Thảo Điền, Quận 2, Hồ Chí Minh<br/>"+
+            "Giờ đóng 22:30<br/>"+
+            "Điện thoại: 090 305 07 10"
+            img.setAttribute('src', '../images/mapq2.jpg')
+            break;
+        case 'q3':
+            p.innerHTML = "Địa chỉ: 23B Ngô Thời Nhiệm, Phường 6, Quận 3, Hồ Chí Minh<br/>" +
+                "Giờ đóng cửa 0:00<br/>" +
+                "Điện thoại: 028 3933 0934";
+            img.setAttribute('src', '../images/mapq3.jpg')
+            break;
+        case 'q6':
+            p.innerHTML = "Địa chỉ: 117A Tân Hòa Đông, Phường 14, Quận 6, Hồ Chí Minh<br/>"+
+            "Mở cả ngày"
+            img.setAttribute('src', '../images/mapq6.jpg')
+            break;
+        case 'q8':
+            p.innerHTML = "Địa chỉ: 907 Tạ Quang Bửu, Phường 5, Quận 8, Hồ Chí Minh<br/>" +
+                "Giờ đóng cửa 22:00<br/>" +
+                "Điện thoại: 028 5430 8448";
+            img.setAttribute('src', '../images/mapq8.jpg')
+            break;
+        case 'q11':
+            p.innerHTML = "Địa chỉ: 70 Lữ Gia, Phường 15, Quận 11, Hồ Chí Minh<br/>" +
+                "Giờ đóng cửa 22:30<br/>" +
+                "Điện thoại: 090 955 99 02h";
+            img.setAttribute('src', '../images/mapq11.jpg')
+            break;
+        case 'qtd':
+            p.innerHTML = "Địa chỉ: 13 Khổng Tử, Bình Thọ, Thủ Đức, Hồ Chí Minh<br/>"+
+            "Giờ đóng cửa 22:15<br/>"+
+            "Điện thoại: 0818 180 518" 
+            img.setAttribute('src', '../images/maptd.jpg')
+            break;
+        default:
+            p.innerHTML = "";
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
